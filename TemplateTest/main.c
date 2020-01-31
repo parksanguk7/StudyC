68380534a1e96fc91c1f20b41d51c3bd1c7655d5
parksanguk <swsw1717@naver.com>
2020-01-31 11:36:24 +0900

    2장 완료

--- TemplateTest/main.c
+++ TemplateTest/main.c
@@ -3,16 +3,17 @@
 #endif // _MSC_VER
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>// 시스템헤더 파일
 
 int main() {
-	printf("Hello World\n");
-	printf("Hi\tsecond again\n");
-	printf("My\tfriend\n");
+	printf("%d\n",100);
+	printf("%lf\n",3.14);
+	printf("%.1lf\n",3.141592);//
+	printf("%.10lf\n",3.1415);
+	printf("%.10lf\n",3.4);
 
-	printf("Goot\bd\tchance\n");
-
-	printf("Cow\rW\a\n");
+	printf("%d과 %d의 합은 %d 입니다.\n", 10, 20, 10 + 20);
+	printf("%.1lf-%.1lf=%.1lf\n", 3.4, 1.2, 3.4 - 1.2);
 	
 	// type here
 
