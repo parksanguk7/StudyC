0ef1eec48bac5db93d0faffe5a70282910c21a7d
parksanguk <swsw1717@naver.com>
2020-01-31 17:23:39 +0900

    Chapter4

--- InputTest/main.c
+++ InputTest/main.c
@@ -6,20 +6,21 @@
 #include <stdlib.h>
 
 int main() {
-	/*int age;
+	int age;
 	double height;
 
 	printf("나이와 키를 입력하세요 :");
 	scanf_s("%d%lf", &age , &height);
-	printf("나이는 %d살, 키는 %0.1lfcm 입니다\n", age, height);*/
+	printf("나이는 %d살, 키는 %0.1lfcm 입니다\n", age, height);
 
 	char grade;
 	char name[20];
+	getchar();
 
 	printf("학점을 입력하세요 :");
 	scanf("%c", &grade);
 	printf("이름을 입력하세요 : ");
-	scanf("%s", name);
+	scanf("%s", &name);
 
 	printf("%s 의 학점 : % c\n", name, grade);
 
